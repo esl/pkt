2cd9300d3455d9e20e29a7b4347960009f45528c
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2014-02-19 15:30:47 +0100

    Use optimized checksum calculation for TCP
    TCP encapsulation uses now more efficient version of calculating
    checksum.
    (Motivated by FlowForwarding/LINC-Switch#293).

--- src/pkt_tcp.erl
+++ src/pkt_tcp.erl
@@ -6,89 +6,63 @@
 
 %%% API -------------------------------------------------------------------------
 
-decapsulate(
-  <<SPort:16, DPort:16,
-    SeqNo:32,
-    AckNo:32,
-    Off:4, 0:4, CWR:1, ECE:1, URG:1, ACK:1,
-    PSH:1, RST:1, SYN:1, FIN:1, Win:16,
-    Sum:16, Urp:16,
-    Rest/binary>>
- ) when Off >= 5 ->
-    {Opt, Payload} = options(Off, Rest),
-    {#tcp{
-        sport = SPort, dport = DPort,
-        seqno = SeqNo,
-        ackno = AckNo,
-        off = Off, cwr = CWR, ece = ECE, urg = URG, ack = ACK,
-        psh = PSH, rst = RST, syn = SYN, fin = FIN, win = Win,
-        sum = Sum, urp = Urp,
-        opt = Opt
-       }, Payload}.
-
 encapsulate(IP, TCP, Payload) ->
-    HdrBin = tcp(TCP#tcp{sum = makesum([IP, TCP, Payload])}),
-    <<HdrBin/binary, Payload/binary>>.
+    UntilChecksum = construct_tcp_binary_until_checksum(TCP),
+    BehindChecksum = construct_tcp_binary_behind_checksum(TCP, Payload),
+    Length = calculate_tcp_length(TCP, Payload),
+    PseudoHeader = construct_tcp_pseudo_header(IP, Length),
+    Checksum = pkt_checksum:compute_internet_checksum(<<PseudoHeader/binary,
+                                                        UntilChecksum/binary,
+                                                        0:16,
+                                                        BehindChecksum/binary>>),
+    <<UntilChecksum/binary, Checksum:16, BehindChecksum/binary>>.
+
+decapsulate(<<SPort:16, DPort:16,
+              SeqNo:32,
+              AckNo:32,
+              Off:4, 0:4, CWR:1, ECE:1, URG:1, ACK:1,
+              PSH:1, RST:1, SYN:1, FIN:1, Win:16,
+              Sum:16, Urp:16,
+              Rest/binary>>
+           ) when Off >= 5 ->
+    {Opt, Payload} = separate_options_from_payload(Off, Rest),
+    {#tcp{sport = SPort, dport = DPort,
+          seqno = SeqNo,
+          ackno = AckNo,
+          off = Off, cwr = CWR, ece = ECE, urg = URG, ack = ACK,
+          psh = PSH, rst = RST, syn = SYN, fin = FIN, win = Win,
+          sum = Sum, urp = Urp,
+          opt = Opt}, Payload}.
 
 %%% Helper functions ------------------------------------------------------------
 
-tcp(#tcp{
-       sport = SPort, dport = DPort,
-       seqno = SeqNo,
-       ackno = AckNo,
-       off = Off, cwr = CWR, ece = ECE, urg = URG, ack = ACK,
-       psh = PSH, rst = RST, syn = SYN, fin = FIN, win = Win,
-       sum = Sum, urp = Urp, opt = Opt
-      }) ->
+construct_tcp_binary_until_checksum(
+  #tcp{
+     sport = SPort, dport = DPort,
+     seqno = SeqNo,
+     ackno = AckNo,
+     off = Off, cwr = CWR, ece = ECE, urg = URG, ack = ACK,
+     psh = PSH, rst = RST, syn = SYN, fin = FIN, win = Win}) ->
     <<SPort:16, DPort:16,
       SeqNo:32,
       AckNo:32,
       Off:4, 0:4, CWR:1, ECE:1, URG:1, ACK:1,
-      PSH:1, RST:1, SYN:1, FIN:1, Win:16,
-      Sum:16, Urp:16, Opt/binary >>.
+      PSH:1, RST:1, SYN:1, FIN:1, Win:16>>.
 
-options(Offset, Payload) ->
-    N = (Offset-5)*4,
-    <<Opt:N/binary, Payload1/binary>> = Payload,
-    {Opt, Payload1}.
+construct_tcp_binary_behind_checksum(#tcp{urp = Urp, opt = Opt}, Payload) ->
+    <<Urp:16, Opt/binary, Payload/binary>>.
 
-makesum(Hdr) -> 16#FFFF - checksum(Hdr).
+calculate_tcp_length(#tcp{opt = Opts}, Payload) ->
+    ?TCPHDRLEN + byte_size(Opts) + byte_size(Payload).
 
-checksum([IP, TransportLayer, Payload]) ->
-    checksum(IP, TransportLayer, Payload);
-checksum(Hdr) ->
-    lists:foldl(fun compl/2, 0, [ W || <<W:16>> <= Hdr ]).
+construct_tcp_pseudo_header(#ipv4{saddr = SrcAddr, daddr = DstAddr},
+                            DatagramLength) ->
+    <<SrcAddr/binary, DstAddr/binary, 0, ?IPPROTO_TCP, DatagramLength:16>>;
+construct_tcp_pseudo_header(#ipv6{saddr = SrcAddr, daddr = DstAddr},
+                            DatagramLength) ->
+    <<SrcAddr/binary, DstAddr/binary, DatagramLength:32, 0:24, ?IPPROTO_TCP>>.
 
-compl(N) when N =< 16#FFFF -> N;
-compl(N) -> (N band 16#FFFF) + (N bsr 16).
-compl(N,S) -> compl(N+S).
-
-checksum(#ipv4{saddr = SAddr,
-               daddr = DAddr},
-         #tcp{} = TCPhdr,
-         Payload) ->
-    TCP = tcp(TCPhdr#tcp{sum = 0}),
-    Len = size(TCP) + size(Payload),
-    Pad = 8 * (Len rem 2),
-    checksum(<<SAddr:32/bits,
-               DAddr:32/bits,
-               0:8, ?IPPROTO_TCP:8,
-               Len:16,
-               TCP/binary,
-               Payload/bits,
-               0:Pad>>);
-
-checksum(#ipv6{saddr = SAddr,
-               daddr = DAddr},
-         #tcp{} = TCPhdr,
-         Payload) ->
-    TCP = tcp(TCPhdr#tcp{sum = 0}),
-    Len = size(TCP) + size(Payload),
-    Pad = 8 * (Len rem 2),
-    checksum(<<SAddr:128/bits,
-               DAddr:128/bits,
-               Len:32,
-               0:24, ?IPPROTO_TCP:8,
-               TCP/binary,
-               Payload/bits,
-               0:Pad>>).
+separate_options_from_payload(Offset, OptsWithPayload) ->
+    OptsBytes = (Offset - 5)*4,
+    <<Opts:OptsBytes/binary, Payload/binary>> = OptsWithPayload,
+    {Opts, Payload}.
